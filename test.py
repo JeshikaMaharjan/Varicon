5751f870f60b58d011fd00bf5e41e9565b880804
Jeshika Maharjan <jeshika99@gmail.com>
2023-11-06 13:46:29 +0545

    pre-commit testing

--- test.py
+++ test.py
@@ -1,1 +1,12 @@
 print("Hello world")
+
+
+def nonpep8function(x, y):
+    result = x + y
+    if result > 5:
+        return print(result)
+    else:
+        return print(result * 2)
+
+
+nonpep8function(3, 3)
