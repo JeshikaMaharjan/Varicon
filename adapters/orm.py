63a6b200ab103288cc1cd696e9660fc504c9e094
Jeshika Maharjan <jeshika99@gmail.com>
2023-11-08 15:27:17 +0545

    changes made

--- adapters/orm.py
+++ adapters/orm.py
@@ -1,52 +1,9 @@
 from database import Database
 
 
-class UserRepository:
-    def __init__(self, database_name):
-        self.db = Database(database_name)
-
-
 class TodoRepository:
     def __init__(self, database_name):
         self.db = Database(database_name)
-
-
-class UserRepositoryImpl(UserRepository):
-    def create(self, name, email, username, password):
-        self.db.cursor.execute(
-            "INSERT INTO users (name, email, username, password) VALUES (?,?,?, ?)",
-            (name, email, username, password),
-        )
-        self.db.conn.commit()
-        return self.db.cursor.lastrowid
-
-    def get(self, user_id):
-        self.db.cursor.execute(
-            "SELECT id, name, email,username, password FROM users WHERE id = ?",
-            (user_id,),
-        )
-        return self.db.cursor.fetchone()
-
-    def get_by_username(self, username):
-        self.db.cursor.execute(
-            "SELECT id, username, password FROM users WHERE username = ?", (username,)
-        )
-        return self.db.cursor.fetchone()
-
-    def update(self, user_id, updated_data):
-        update_query = "UPDATE users SET {} WHERE id = ?".format(
-            ", ".join(f"{key} = ?" for key in updated_data.keys())
-        )
-        values = list(updated_data.values())
-        values.append(user_id)
-        self.db.cursor.execute(update_query, values)
-        self.db.conn.commit()
-        return self.db.cursor.rowcount > 0
-
-    def delete(self, user_id):
-        self.db.cursor.execute("DELETE FROM users WHERE id = ?", (user_id,))
-        self.db.conn.commit()
-        return self.db.cursor.rowcount > 0
 
 
 class TodoRepositoryImpl(TodoRepository):
