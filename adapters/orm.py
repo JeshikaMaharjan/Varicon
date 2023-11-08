1ddffb6fc0b65fba268111161e743092a6ab7f0d
Jeshika Maharjan <jeshika99@gmail.com>
2023-11-08 16:35:55 +0545

    Refactoring

--- adapters/orm.py
+++ adapters/orm.py
@@ -3,16 +3,6 @@ from django.db import models
 # Create your models here.
 
 
-class Users(models.Model):
-    username = models.CharField(max_length=50)
-    firstname = models.CharField(max_length=50)
-    lastname = models.CharField(max_length=50)
-    passwd = models.CharField(max_length=50)
-
-    def __str__(self):
-        return f"{self.username}"
-
-
 class Tasks(models.Model):
     task = models.CharField(max_length=50)
     priority = models.IntegerField()
